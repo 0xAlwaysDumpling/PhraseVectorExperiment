ad7f773d8cc157d4854db3fb6c4b39b3492f95ca
Johnny Tan <jhnnytny@gmail.com>
2015-12-10 20:53:43 -0500

    delmiter added

--- Lasange/Util/generatePhraseVocab.py
+++ Lasange/Util/generatePhraseVocab.py
@@ -9,7 +9,14 @@ import sys
 
 input_file = sys.argv[1]
 skip = sys.argv[2]
+delimit = sys.argv[3] #if 0 then space if 1 then tab
 out_file = 'skip.' + str(skip) +'.vocab.txt'
+
+if int(delimit) == 0:
+    delimiter = ' '
+else:
+    delimiter = '\t'
+
 
 vocab_size = 0
 lineCount = 0
@@ -18,7 +25,7 @@ with open(out_file, "w") as w:
     with open(input_file, "r") as f:
         for line in f.readlines():
             if lineCount == int(skip):
-                line = line.split(' ')
+                line = line.split(delimiter)
                 word = line[0]
                 words = word.split('_')
                 lineCount = 0
